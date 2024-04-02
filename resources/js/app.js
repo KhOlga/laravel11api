94b510b23059f366069619993900ff4c05d90d36
Olha Kharabet <kharabet.olga@gmail.com>
2024-04-02 18:08:06 +0200

    Added Vue, Breeze

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+
+import { createApp } from 'vue';
+import ProductsIndex from './components/Products/Index.vue';
+
+createApp({})
+	.component('ProductsIndex', ProductsIndex)
+	.mount('#app');
